8daf47bde1cc8d1cb8950e8992006003f2d6aeac
Michael Willis <michael.willis@digital.justice.gov.uk>
2021-03-10 11:42:41 +0000

    DT-1716 Add court data

--- src/main/resources/db/migration/V1_5__new_court_table.sql
+++ src/main/resources/db/migration/V1_5__new_court_table.sql
@@ -1,23 +1,38 @@
 CREATE TABLE court_detail
 (
-    id                    VARCHAR(6)   NOT NULL PRIMARY KEY,
-    court_name            VARCHAR(80)  NOT NULL,
-    court_description     VARCHAR(200),
-    type_code             VARCHAR(3)   NOT NULL,
-    BUILDING_NAME         VARCHAR(35)  NULL,
-    STREET                VARCHAR(35)  NULL,
-    LOCALITY              VARCHAR(35)  NULL,
-    TOWN                  VARCHAR(35)  NULL,
-    COUNTY                VARCHAR(35)  NULL,
-    POSTCODE              VARCHAR(8)   NULL,
-    COUNTRY               VARCHAR(16)  NULL,
-    TELEPHONE_NUMBER      VARCHAR(35)  NULL,
-    FAX_NUMBER            VARCHAR(35)  NULL,
-    EMAIL_ADDRESS         VARCHAR(255) NULL,
-    CREATED_DATETIME      TIMESTAMP    NOT NULL,
-    CREATED_BY_USER_ID    VARCHAR(60)  NOT NULL,
-    LAST_UPDATED_DATETIME TIMESTAMP    NOT NULL,
-    LAST_UPDATED_USER_ID  VARCHAR(60)  NOT NULL
+    id                      VARCHAR(6)   NOT NULL PRIMARY KEY,
+    court_name              VARCHAR(80)  NOT NULL,
+    court_description       VARCHAR(200),
+    type                    VARCHAR(3)   NOT NULL,
+    active                  BOOLEAN      NOT NULL,
+    created_datetime        TIMESTAMP    NOT NULL,
+    last_updated_datetime   TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE building
+(
+    id                      SERIAL       PRIMARY KEY,
+    court_code              VARCHAR(6)   NOT NULL,
+    sub_code                VARCHAR(6)   NULL,
+    building_name           VARCHAR(50)  NULL,
+    street                  VARCHAR(80)  NULL,
+    locality                VARCHAR(80)  NULL,
+    town                    VARCHAR(80)  NULL,
+    county                  VARCHAR(80)  NULL,
+    postcode                VARCHAR(8)   NULL,
+    country                 VARCHAR(16)  NULL,
+    created_datetime        TIMESTAMP    NOT NULL,
+    last_updated_datetime   TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE contact
+(
+    id                      SERIAL       PRIMARY KEY,
+    building_id             INTEGER      NOT NULL,
+    type                    VARCHAR(5)   NOT NULL,
+    detail                  VARCHAR(80)  NULL,
+    created_datetime        TIMESTAMP    NOT NULL,
+    last_updated_datetime   TIMESTAMP    NOT NULL
 );
 
 CREATE TABLE court_type
@@ -26,7 +41,10 @@ CREATE TABLE court_type
     description VARCHAR(80) NOT NULL
 );
 
-ALTER TABLE court_detail ADD FOREIGN KEY (type_code) REFERENCES court_type(id);
+ALTER TABLE court_detail ADD FOREIGN KEY (type) REFERENCES court_type(id);
+ALTER TABLE building ADD FOREIGN KEY (court_code) REFERENCES court_detail(id);
+ALTER TABLE contact ADD FOREIGN KEY (building_id) REFERENCES building(id);
 
-CREATE INDEX court_detail_idx ON court_detail (type_code);
+CREATE INDEX court_detail_idx ON court_detail (type);
+CREATE UNIQUE INDEX building_udx ON building (court_code, sub_code);
 
