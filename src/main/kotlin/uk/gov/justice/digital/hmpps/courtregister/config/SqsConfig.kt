978ad991ac3024c4b23492e8240e2ef53bda753d
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-03-10 09:13:22 +0000

    DT-1632 PR comments and aws bean fix (#23)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtregister/config/SqsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtregister/config/SqsConfig.kt
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class SqsConfig {
 
   @Bean
+  @Primary
   @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
   fun awsSqsClient(
     @Value("\${sqs.aws.access.key.id}") accessKey: String,
