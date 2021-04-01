da4aa56dc136d61b4a2001889ba3cba845004055
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-01 11:58:55 +0100

    DT-1829 Upgrades (#33)
    * DT-1829 useLatestVersions
    
    * DT-1829 upgrade kotlin spring version
    
    * Upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
-  kotlin("plugin.spring") version "1.4.30"
-  kotlin("plugin.jpa") version "1.3.61"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
+  kotlin("plugin.spring") version "1.4.32"
+  kotlin("plugin.jpa") version "1.4.32"
   idea
 }
 
@@ -22,29 +22,29 @@ dependencies {
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.5.3")
+  runtimeOnly("org.flywaydb:flyway-core:7.7.1")
   runtimeOnly("org.postgresql:postgresql:42.2.19")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.1")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.958"))
-  implementation("com.amazonaws:aws-java-sdk-sns:1.11.958")
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.989"))
+  implementation("com.amazonaws:aws-java-sdk-sns:1.11.989")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.24.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
   testImplementation("org.testcontainers:localstack:1.15.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.4.2")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.4.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:3.7.7")
+  testImplementation("org.mockito:mockito-inline:3.8.0")
 }
