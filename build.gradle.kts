e65e5a23866cbea7fe4029b5f1caa53e706c0f22
Peter Phillips <peter@greenthistle.co.uk>
2021-04-20 11:53:59 +0100

    DT-1897: ✨ Upgrade to spring boot 2.4.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.7"
   kotlin("plugin.spring") version "1.4.32"
   kotlin("plugin.jpa") version "1.4.32"
   idea
@@ -22,29 +22,37 @@ dependencies {
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.7.1")
+  runtimeOnly("org.flywaydb:flyway-core:7.8.1")
   runtimeOnly("org.postgresql:postgresql:42.2.19")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.code.gson:gson:2.8.6")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.7")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.7")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.7")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.989"))
-  implementation("com.amazonaws:aws-java-sdk-sns:1.11.989")
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1001"))
+  implementation("com.amazonaws:aws-java-sdk-sns:1.11.1001")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
-  testImplementation("org.testcontainers:localstack:1.15.2")
+  testImplementation("org.testcontainers:localstack:1.15.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.4.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:3.8.0")
+  testImplementation("org.mockito:mockito-inline:3.9.0")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions {
+      jvmTarget = "15"
+    }
+  }
 }
