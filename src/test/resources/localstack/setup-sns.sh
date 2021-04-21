cde2e178a96bcb2f13be60f202cfeb3da7d3e3f4
Peter Phillips <peter@greenthistle.co.uk>
2021-04-21 10:49:05 +0100

    DT-1949: ⬆️ Update to latest localstack

--- src/test/resources/localstack/setup-sns.sh
+++ src/test/resources/localstack/setup-sns.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
-aws --endpoint-url=http://localhost:4575 sns create-topic --name hmpps_domain_events
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name hmpps_audit_queue
+aws --endpoint-url=http://localhost:4566 sns create-topic --name hmpps_domain_events
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name hmpps_audit_queue
 
 echo All Ready
