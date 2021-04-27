421a10b79f5ca9d96819ec9c918a5239617e4958
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-04-27 14:48:48 +0100

    DT-1954 Remove constraint until we are testing against a Postgres DB (#44)

--- src/main/resources/db/migration/V1_12__buidlidng_constraints.sql
+++ src/main/resources/db/migration/V1_12__buidlidng_constraints.sql
@@ -1,3 +1,13 @@
 DROP INDEX building_udx;
 CREATE UNIQUE INDEX building_udx ON building (sub_code);
-ALTER table building ADD CHECK(NOT EXISTS (SELECT 1 FROM court c WHERE c.id = sub_code));
+-- TODO replace the ALTER statement with the function and constraint below once we have switched to Postgres
+-- ALTER table building ADD CHECK(NOT EXISTS (SELECT 1 FROM court c WHERE c.id = sub_code));
+
+-- create function court_code_exists(court_code varchar) returns boolean as
+-- $$
+-- begin
+--     return exists(select 1 from court c where c.id = court_code);
+-- end
+-- $$ language plpgsql;
+-- ALTER table building ADD constraint sub_code_already_exists check(not court_code_exists(sub_code));
+
