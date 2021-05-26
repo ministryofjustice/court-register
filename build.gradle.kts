241ba016485c0991a3656735b93d4e6bc470e40c
petergphillips <peter@greenthistle.co.uk>
2021-05-26 10:12:32 +0100

    pgp DT 2070 spring boot upgrade (#63)
    * DT-2070: ⬆️ Upgrade to spring boot 2.5.0 and kotlin 1.5.10
    
    * DT-2070: ⬆️ Upgrade to 3.3.0 of gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
-  kotlin("plugin.spring") version "1.5.0"
-  kotlin("plugin.jpa") version "1.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
+  kotlin("plugin.spring") version "1.5.10"
+  kotlin("plugin.jpa") version "1.5.10"
   idea
 }
 
@@ -22,27 +22,27 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.flywaydb:flyway-core:7.9.0")
+  implementation("org.flywaydb:flyway-core:7.9.1")
   runtimeOnly("org.postgresql:postgresql:42.2.20")
   implementation("com.vladmihalcea:hibernate-types-52:2.11.1")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.9")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1020"))
-  implementation("com.amazonaws:aws-java-sdk-sns:1.11.1020")
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1025"))
+  implementation("com.amazonaws:aws-java-sdk-sns:1.11.1025")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.26.0")
   testImplementation("org.testcontainers:localstack:1.15.3")
   testImplementation("org.testcontainers:postgresql:1.15.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
