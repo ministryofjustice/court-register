09dde694fe96c459077f5f2abedf32b805cf7ca1
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2021-12-16 16:31:43 +0000

    DT-2953: updated to use mockito-kotlin instead of com.nhaarman.mockitokotlin2 (#103)

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.2")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:4.0.0")
+  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 java {
