cc097b1633448bdedbe3afbb505d43fe2df2a9cc
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-01-12 07:49:24 +0000

    DT-3026: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.1 (#105)
    includes springboot 2.6.2 and kotlin 1.6.10 and jvmTarget to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
-  kotlin("plugin.spring") version "1.6.0"
-  kotlin("plugin.jpa") version "1.6.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
   idea
 }
 
@@ -23,7 +23,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.flywaydb:flyway-core:8.2.2")
+  implementation("org.flywaydb:flyway-core:8.4.1")
   runtimeOnly("org.postgresql:postgresql:42.3.1")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
@@ -31,14 +31,14 @@ dependencies {
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.code.gson:gson:2.8.9")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.1")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.129"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.137"))
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
@@ -46,18 +46,17 @@ dependencies {
   testImplementation("org.testcontainers:localstack:1.16.2")
   testImplementation("org.testcontainers:postgresql:1.16.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.5.2")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("org.mockito:mockito-inline:4.2.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.jvmTarget = "17"
   }
 }
