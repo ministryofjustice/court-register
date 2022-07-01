d48720f3d05794eb90134910f122e601c466ed4a
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-07-01 15:36:46 +0100

    HAAR-600: update to hmpps-spring-boot-sqs 1.1.6 (#134)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.6-beta")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
   implementation("org.flywaydb:flyway-core:8.5.13")
   runtimeOnly("org.postgresql:postgresql:42.4.0")
-  implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.3")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
@@ -38,13 +38,13 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.248"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.252"))
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
-  testImplementation("org.testcontainers:localstack:1.17.2")
-  testImplementation("org.testcontainers:postgresql:1.17.2")
+  testImplementation("org.testcontainers:localstack:1.17.3")
+  testImplementation("org.testcontainers:postgresql:1.17.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.3")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
