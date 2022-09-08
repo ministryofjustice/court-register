bed1d9ef602d59c61c94c026d8fa44d4f5fbf36f
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-09-08 14:42:27 +0100

    HAAR-790: ⬆️ upgrade gradle-spring-boot plugin to 4.5.0-beta (#147)
    * HAAR-790: ⬆️ upgrade gradle-spring-boot plugin to 4.5.0-beta
    
    * HAAR-790: ⬆️ remove flyway version so not pinned

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,22 +23,22 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.8")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.flywaydb:flyway-core:8.5.13")
-  runtimeOnly("org.postgresql:postgresql:42.4.1")
-  implementation("com.vladmihalcea:hibernate-types-52:2.17.3")
+  implementation("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:postgresql:42.5.0")
+  implementation("com.vladmihalcea:hibernate-types-52:2.19.0")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.code.gson:gson:2.9.1")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.11")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.277"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.298"))
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
@@ -48,7 +48,7 @@ dependencies {
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.4")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.6.1")
+  testImplementation("org.mockito:mockito-inline:4.8.0")
 }
 
 java {
